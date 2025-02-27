3dfafee8ba688611a65c5913250416391d887adc
MinhHoang200709 <mhoangdev20@gmail.com>
2025-02-27 21:34:10 +0700

    LKG_delete_header.js

--- Scripts/LKG_delete_header.js
+++ Scripts/LKG_delete_header.js
@@ -1,4 +1,4 @@
+const version = 'V1.0.2';
 
-$request.headers['X-Custom-Header'] = undefined; 
 
-$done();
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
